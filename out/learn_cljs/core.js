011e04278878e8a6c9d33ac11d99660fe8d713ed
Espen Trydal <espen@trydal.io>
2022-08-12 10:30:59 +0000

    hello world

---
+++ out/learn_cljs/core.js
@@ -0,0 +1,6 @@
+// Compiled by ClojureScript 1.11.54 {:optimizations :none}
+goog.provide('learn_cljs.core');
+goog.require('cljs.core');
+alert("Hei p\u00E5 deg");
+
+//# sourceMappingURL=core.js.map
